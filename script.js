93747eed03e2671b2bbd2154a6dc5ae92fecb0e8
snigdhapushpendra <131598402+snigdhapushpendra@users.noreply.github.com>
2024-07-04 18:06:23 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+    });
+});
